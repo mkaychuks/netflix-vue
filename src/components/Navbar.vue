e7b0bfdcb8f422e1155683c7963280f5f74b3c14
mkaychuks <sachufusi@gmail.com>
2025-04-03 20:39:16 +0100

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="flex justify-between items-baseline p-4 z-[100] w-full absolute">
+    <!-- the home logo -->
+    <RouterLink to="/">
+      <h1 class="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
+    </RouterLink>
+    <!-- the CTA login and sign up -->
+    <div>
+      <RouterLink to="/account">
+        <button className="text-white pr-4">Account</button>
+      </RouterLink>
+      <RouterLink to="/signup">
+        <button
+          className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white"
+        >
+          Sign Up
+        </button>
+      </RouterLink>
+    </div>
+  </div>
+</template>
